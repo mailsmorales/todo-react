6eaf3d95e92092cc5748c39e0d807e7b0f672fbd
mukhamed-Ali <mailsmorales21@mail.ru>
2022-08-10 00:46:32 +0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import React from "react";
-import {AiOutlinePlus} from 'react-icons/ai'
+import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import Todo from "./Todo";
 
 const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`
-}
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+};
 
 function App() {
-  return <div className={style.bg}>
-    <div className={style.container}>
-      <h3 className={style.heading}>Todo App</h3>
-      <form className={style.form}>
-        <input className={style.input} type="text" placeholder="Add Todo" />
-        <button className={style.btn}><AiOutlinePlus size={30} /></button>
-      </form>
-      <ul>
-        
-      </ul>
+  const [todos, setTodos] = useState(["Learn React", "Grind Leetcode"]);
+
+  return (
+    <div className={style.bg}>
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add Todo" />
+          <button className={style.btn}>
+            <AiOutlinePlus size={30} />
+          </button>
+        </form>
+        <ul>
+          <Todo />
+        </ul>
+      </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
---
+++ src/Todo.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const style = {
+ 
+};
+
+const Todo = () => {
+  return <li className={style.li}>
+    <div className={style.row}>
+      <input type="checkbox" />
+    </div>
+  </li>;
+};
+
+export default Todo;
