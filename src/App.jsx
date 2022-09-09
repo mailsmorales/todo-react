5b3093be21db32061d1c8531b9fc19aca44839a1
mukhamed-Ali <mailsmorales21@mail.ru>
2022-09-09 10:36:23 +0600

    style app form

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Todo from "./Todo";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-  container: `bg-slate-100`
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2 `,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`
 };
 
 function App() {
@@ -17,7 +22,7 @@ function App() {
         <h3 className={style.heading}>Todo App</h3>
         <form className={style.form}>
           <input className={style.input} type="text" placeholder="Add Todo" />
-          <button className={style.btn}>
+          <button className={style.button}>
             <AiOutlinePlus size={30} />
           </button>
         </form>
